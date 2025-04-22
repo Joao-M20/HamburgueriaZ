2f24afac6eb6754a9337361761a0bd48cc8e0812
ivanm <ivanmayta2585@gmail.com>
2025-04-21 21:46:41 -0300

    Function Resume

--- app/src/main/java/com/example/hamburgueriaz/MainActivity.kt
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.example.hamburgueriaz
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
     var contador = 0
     lateinit var quantidade: TextView
 
-    val nome: EditText = findViewById(R.id.editTextNome)
-    val bacon: CheckBox = findViewById(R.id.checkBoxBacon)
-    val queijo: CheckBox = findViewById(R.id.checkBoxQueijo)
-    val onion: CheckBox = findViewById(R.id.checkBoxOnion)
-
-    val menos: Button = findViewById(R.id.buttonMenos)
-    val mais: Button = findViewById(R.id.buttonMais)
-
-
-    val resumo: TextView = findViewById(R.id.textViewResumo)
-    val pedido: Button = findViewById(R.id.buttonPedido)
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-
-
-        quantidade.text = contador.toString()
         quantidade = findViewById(R.id.textViewQuantidade)
-
-
-
+        quantidade.text = contador.toString()
 
 
     }
@@ -57,13 +37,73 @@ class MainActivity : AppCompatActivity() {
 
         } else{
             contador--
-            quantidade.text = contador.toString()    
+            quantidade.text = contador.toString()
         }
     }
 
-    fun EnviarPedido(view: View){
-        val nomePedido = nome.text
+
+
+    fun EnviarPedido(view: View) {
+
+
+        val nome: EditText = findViewById(R.id.editTextNome)
+        val bacon: CheckBox = findViewById(R.id.checkBoxBacon)
+        val queijo: CheckBox = findViewById(R.id.checkBoxQueijo)
+        val onion: CheckBox = findViewById(R.id.checkBoxOnion)
+        val resumo: TextView = findViewById(R.id.textViewResumo)
+        val valorFinal: TextView = findViewById(R.id.textViewValorFinal)
+        val nomeCliente = nome.text.toString()
+        val temBacon = bacon.isChecked
+        val temQueijo = queijo.isChecked
+        val temOnion = onion.isChecked
+        val meuEmail = "ivanmayta2585@gmail.com"
+        val assunto = "oi"
+        val nomeAssunto = nome.text.toString()
+
+
+        val precoTotal = calcularPreco(temBacon, temQueijo, temOnion, contador)
+
+
+        val mensagem = """
+        Nome do cliente: $nomeCliente
+        Tem Bacon? ${if (temBacon) "Sim" else "Não"}
+        Tem Queijo? ${if (temQueijo) "Sim" else "Não"}
+        Tem Onion Rings? ${if (temOnion) "Sim" else "Não"}
+        Quantidade: $contador
+        Preço final: R$ %.2f
+        """.trimIndent().format(precoTotal)
+
+
+        resumo.text = mensagem
+        valorFinal.text = "R$ %.2f".format(precoTotal)
+
+
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(meuEmail))
+            putExtra(Intent.EXTRA_SUBJECT, "Pedido de $nomeAssunto")
+            putExtra(Intent.EXTRA_TEXT, mensagem)
+        }
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(Intent.createChooser(intent, "Send Email"))
+        } else {
+            Toast.makeText(this, "Nenhum app de e-mail encontrado.", Toast.LENGTH_SHORT).show()
+        }
 
     }
+
+    fun calcularPreco(bacon: Boolean, queijo: Boolean, onion: Boolean, quantidade: Int): Double {
+        val precoBase = 20.0
+        var precoAdicionais = 0.0
+
+        if (bacon) precoAdicionais += 2.0
+        if (queijo) precoAdicionais += 2.0
+        if (onion) precoAdicionais += 3.0
+
+        return (precoBase + precoAdicionais) * quantidade
+    }
+
+
 
 }
